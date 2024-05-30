3def75073a12554e2c733cd817fe1c0dd33b6198
ju57_4_8361nn3r <xuan2103yyx@gmail.com>
2024-05-30 15:09:34 +0800

    yay

--- heap/src/components/login.jsx
+++ heap/src/components/login.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import "./css/Login.css";
+import UserService from "../services/UserService";
 
 class Login extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    // this.loginSubmit = this.loginSubmit.bind(this);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.changeUsernameHandler = this.changeUsernameHandler.bind(this);
+    this.changePasswordHandler = this.changePasswordHandler.bind(this);
+    this.loginSubmit = this.loginSubmit.bind(this);
   }
 
   componentDidMount() {
 
+  }
+
+  loginSubmit = (event) => {
+    event.preventDefault();
+    let credentials = {username: this.state.username, password: this.state.password};
+    UserService.loginUser(credentials).then(res=>{
+      this.props.history.push('/profile')
+    })
+  }
+
+  changeUsernameHandler= (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  changePasswordHandler= (event) => {
+    this.setState({password: event.target.value});
+  }
+
+  register() {
+    this.props.history.push('/signup')
   }
 
   render() {
@@ -21,16 +47,17 @@ class Login extends Component {
           <form>
             <label>
               <p>Username</p>
-              <input type="text" />
+              <input type="text" value={this.state.username} onChange={this.changeUsernameHandler}/>
             </label>
             <label>
               <p>Password</p>
-              <input type="password" />
+              <input type="password" value={this.state.password} onChange={this.changePasswordHandler}/>
             </label>
             <div className="button-container">
-              <button className="btn btn-wide">Login</button>
+              <button className="btn btn-wide" onClick={this.loginSubmit}>Login</button>
             </div>
           </form>
+
         </div>
       </>
     );
--- heap/src/components/signup.jsx
+++ heap/src/components/signup.jsx
@@ -5,7 +5,39 @@ class Signup extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            name: '',
+            contactNo: '',
+            email: '',
+            password: ''
+        };
+
+        this.changeNameHandler = this.changeNameHandler.bind(this);
+        this.changeContactNoHandler = this.changeContactNoHandler.bind(this);
+        this.changeEmailHandler = this.changeEmailHandler.bind(this);
+        this.changePasswordHandler = this.changePasswordHandler.bind(this);
+    }
+
+    createUser = (e) => {
+        e.preventDefault();
+        let user = {name: this.state.name, contactNo: this.state.contactNo, email: this.state.email, password: this.state.password};
+        console.log('user => ' + JSON.stringify(user));
+    }
+
+    changeNameHandler= (event) => {
+        this.setState({name: event.target.value});
+    }
+
+    changeContactNoHandler= (event) => {
+        this.setState({contactNo: event.target.value});
+    }
+
+    changeEmailHandler= (event) => {
+        this.setState({email: event.target.value});
+    }
+
+    changePasswordHandler= (event) => {
+        this.setState({password: event.target.value});
     }
 
     render() {
@@ -17,19 +49,19 @@ class Signup extends Component {
                 <form>
                     <label>
                         <p>Name</p>
-                        <input type="text" />
+                        <input type="text" value={this.state.name} onChange={this.changeNameHandler} />
                     </label>
                     <label>
                         <p>Contact Number</p>
-                        <input type="tel" pattern="[0-9]{4}-[0-9]{4}" required/>
+                        <input type="tel" pattern="[0-9]{4}-[0-9]{4}" required value={this.state.contactNo} onChange={this.changeContactNoHandler} />
                     </label>
                     <label>
                         <p>Email Address</p>
-                        <input type="email" />
+                        <input type="email" value={this.state.email} onChange={this.changeEmailHandler} />
                     </label>
                     <label>
                         <p>Password</p>
-                        <input type="password" />
+                        <input type="password" value={this.state.password} onChange={this.changePasswordHandler} />
                     </label>
                     <div className="button-container">
                         <button className="btn btn-wide">Sign Up</button>
